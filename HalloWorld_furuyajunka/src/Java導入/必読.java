e5a6c9b1ad2d3e9a5f6af91f1c86ee75a438840e
junju <junju@LAPTOP-JMD3O2V8>
2025-12-08 18:14:00 +0900

    Java導入※必読

---
+++ HalloWorld_furuyajunka/src/Java導入/必読.java
@@ -0,0 +1,12 @@
+package Java導入;
+
+public class 必読 {
+	public static void main(String[] args) {
+String name = "古屋潤佳";
+int age = 25;
+String hobby = "散歩";
+
+System.out.println("私の名前は" + name + "です。");
+System.out.println("年齢は" + age + "です。");
+System.out.println("趣味は" + hobby + "です。");
+	}}
