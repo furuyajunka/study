6c8b494a1335c1cbaa5b8a6a07c95085b37a8c59
junju <junju@LAPTOP-JMD3O2V8>
2026-01-06 19:00:13 +0900

    カリキュラム

--- HalloWorld_furuyajunka/src/curriculum_A/Question1.java
+++ HalloWorld_furuyajunka/src/curriculum_A/Question1.java
@@ -18,59 +18,11 @@ public class Question1 {
 		System.out.println(byteNum*2);
 		System.out.println(letter+" "+letters+" "+isBoolean );
 		System.out.println(byteNum*3+shortNum+intNum+longNum);
-		System.out.println((double)byteNum*shortNum*intNum*longNum);
+		System.out.println((double)byteNum*shortNum*intNum*longNum);//←1E+10ではなく1.0E10という表記になる
 		System.out.println(doubleNum/shortNum);
 		System.out.println(byteNum-shortNum);
 		
 		//Q4
-		String name = "山田太郎";
-		System.out.println("こんにちは"+name+"さん！");
-		
-		//Q5
-		int age = 25;
-		System.out.println("年齢:"+age+"歳");
-		
-		//Q6
-		int num1 = 10;
-		int num2 = 5;
-		int sum =(num1+num2);
-		System.out.println(sum);
-		
-		//Q7
-		int score = 80;
-		score = score + 20;
-		System.out.println("最終スコア:" + score);
-		
-		//Q8		
-		double price = 99.99;
-		int shortprice = (int) price;
-		System.out.println("整数価格:"+shortprice);
-		
-		//Q9
-		String numStr = "123";
-		int number = Integer.parseInt(numStr);
-		System.out.println(number + 10);
-		
-		//Q10
-		int num = 50;
-		String str = String.valueOf(num);
-		System.out.println("得点：" + str + "点");
-		
-		//Q11 
-		int a = 10;
-		int b = 20;
-		boolean result = ( a < b );
-		System.out.println(result);
-		
-		//Q12  
-		int x = 15;
-		System.out.println(x >= 10 ? "OK" : "NG");
-
-		//Q13
-		String text = "私はJavaが好きです。Javaは楽しい！";
-		String text1 = text.replace("Java", "Python");
-		System.out.println(text1);
-
 	}
 }
 	
